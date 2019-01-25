7ebc673490fd9e6bd330ba4b241e1b0abf1b2825
christine <christine.raouf88@gmail.com>
2019-01-25 12:30:34 +0100

    utilities file

--- parsing_parallel_data.py
+++ parsing_parallel_data.py
@@ -1,9 +1,10 @@
 import re
 
 #split line by line and replace the tokens in the lines
-def read_file_line_by_line(file, file_es, file_en):
+def read_file_line_by_line_with_special_handling(file, file_es, file_en_coref, file_en):
     file_es = open(file_es, 'w')
     file_en = open(file_en, 'w')
+    file_en_coref  = open(file_en_coref, 'w')
     with open(file) as f:
         for line in f:
 
@@ -11,10 +12,12 @@ def read_file_line_by_line(file, file_es, file_en):
             print (lan [0])
             file_es.write(lan[0]+'\n')
             print(lan[1])
-            #replace a regex
+            file_en_coref.write(lan[1]+'\n')
+            #replace a regex of the coref
             print(re.sub("<b_crf>.*?<e_crf> ", "", lan[1]))
             en_line = re.sub("<b_crf>.*?<e_crf> ", "", lan[1])
             file_en.write(en_line+'\n')
+
             print ('**')
 
 
@@ -34,4 +37,7 @@ def read_file_line_by_line(file, file_es, file_en):
             print ('**')
 
 
-read_file_line_by_line('/home/christine/DocTrans_2/training1/training.en-es.sorted.tc.60.both','/home/christine/DocTrans_2/training1/training.en-es.sorted.tc.60.both.es','/home/christine/DocTrans_2/training1/training.en-es.sorted.tc.60.both.en')
+read_file_line_by_line_with_special_handling('/home/christine/DocTrans_2/training1/training.en-es.sorted.tc.60.both','/home/christine/DocTrans_2/training1/training.en-es.sorted.tc.60.es','/home/christine/DocTrans_2/training1/training.en-es.sorted.tc.60.coref.en','/home/christine/DocTrans_2/training1/training.en-es.sorted.tc.60.en')
+read_file_line_by_line_with_special_handling('/home/christine/DocTrans_2/training1/validation.en-es.sorted.tc.60.both','/home/christine/DocTrans_2/training1/validation.en-es.sorted.tc.60.es','/home/christine/DocTrans_2/training1/validation.en-es.sorted.tc.60.coref.en','/home/christine/DocTrans_2/training1/validation.en-es.sorted.tc.60.en')
+
+read_file_line_by_line_with_special_handling('/home/christine/DocTrans_2/training1/test.en-es.sorted.tc.60.both','/home/christine/DocTrans_2/training1/test.en-es.sorted.tc.60.es','/home/christine/DocTrans_2/training1/test.en-es.sorted.tc.60.both.coref.en','/home/christine/DocTrans_2/training1/test.en-es.sorted.tc.60.en')
