c0c050fe8d7e127a352c56f3f47fde3a5f66c3dc
christine <christine.raouf88@gmail.com>
2019-03-01 14:47:35 +0100

    to prepare parallel corpus FOR ADRIAN'S IDEA

--- prepare_corpus_document_level.py
+++ prepare_corpus_document_level.py
@@ -1,11 +1,13 @@
 
 
 
-def make_new_corpus_of_two_lines(file):
+def make_new_corpus_of_two_lines(file, file_path_to_write):
     file_read = open(file, 'r')
+    file_to_write = open(file_path_to_write, 'w')
     keep_second_line = ''
     line1 = file_read.readline().strip().replace('\n','')
     line = file_read.readline().strip().replace('\n','')
+    file_to_write.write(line1+' <sep> '+line)
     print(line1+' <sep> '+line)
     while True:
         keep_last_line= line
@@ -13,9 +15,10 @@ def make_new_corpus_of_two_lines(file):
         line = file_read.readline().strip().replace('\n','')
 
         print(keep_last_line + ' <sep> '+ line)
+        file_to_write.write(keep_last_line + ' <sep> '+ line)
         if not line: break  # EOF
 
 
 
 
-make_new_corpus_of_two_lines('news-original.en')
+make_new_corpus_of_two_lines('news-original.en','')
